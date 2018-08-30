e0c135ad6ac1294011a9d58c7d7b77677230e95a
Shivam Agrawal <Shivam.Agrawal@lntinfotech.com>
2018-08-30 15:08:32 +0530

    Added lemmatizer in nlp

--- nlp.py
+++ nlp.py
@@ -1,16 +1,30 @@
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import WordNetLemmatizer
+
 
 class Lang:
     
     def __init__(self):
+        self.lemmatizer= WordNetLemmatizer()
         print("[ NLP Inilized ]")
         
     def print(self):
         print("Raw data is "+self.msg)
         
-    def process(self, msg):
+    def setMsg(self, msg):
         self.msg= msg
-        self.txt= msg
-        self.sents= sent_tokenize(msg)
-        self.words= word_tokenize(msg)
-        pass
+        self.process()
+    
+    def process(self):
+        self.txt= self.msg
+        self.sents= sent_tokenize(self.msg)
+        self.words= word_tokenize(self.msg)
+        
+    def lemma(self):
+        return self.lemmatizer.lemmatize(self.msg)
+    
+
+if __name__=='__main__':
+    lang= Lang()
+    lang.setMsg("oxes")
+    print(lang.lemma())
